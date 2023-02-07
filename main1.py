83e8c935d7f7d47c052f217b851ac59d2e32086a
TrevorRichardson0480 <trevor.richardson0480@gmail.com>
2023-02-06 23:46:27 -0500

    setup workshop

---
+++ main1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get('/') # this is called a decorator
+def index():  # your function goes directly under the decorator
+    return {'key' : 'value'} # here I return a dictionary
+
+@app.get('/test')
+def get_test():
+    return "hello world" # here I return text
+
+@app.get('/square/{num}')
+def get_square(num: int):
+    return pow(num, 2) # here I return an int
+
+class InputObj(BaseModel):
+    num: int
+
+@app.post('/square')
+def post_square(input: InputObj): 
+    return pow((input.dict()['num']), 2)
